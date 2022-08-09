311ba6311b38790250b9ef9a2009e551784c0883
awais2075 <awais2075@gmail.com>
2022-08-09 10:39:17 +0500

    initial commit

---
+++ src/main/java/com/example/bank/transaction/sample/BankTransactionSampleApplication.java
@@ -0,0 +1,13 @@
+package com.example.bank.transaction.sample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankTransactionSampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BankTransactionSampleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/bank/transaction/sample/BankTransactionSampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.bank.transaction.sample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankTransactionSampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
